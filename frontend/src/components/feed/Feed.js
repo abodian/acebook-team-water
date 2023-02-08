32e8056426f9ade9e4a7b156c0c74612a8f6ae60
David Scott <sirdavidscott@hotmail.com>
2023-02-08 23:48:39 +0000

    managed to get the post page to post a message

--- frontend/src/components/feed/Feed.js
+++ frontend/src/components/feed/Feed.js
@@ -28,18 +28,49 @@ const Feed = ({ navigate }) => {
 
   const post = () => {};
 
+///////////////////////////////
+
+const [message, setMessage] = useState("");
+
+const handleSubmitPost = async (event) => {
+  // event.preventDefault(); this line prevents the page not reloading
+
+  fetch( '/posts', {
+    method: 'post',
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ message: message })
+  })
+    .then(response => {
+      if(response.status === 201) {
+        navigate('/posts')
+      } else {
+        navigate('/signup')
+      }
+    })
+}
+
+
+/////////////////////////
+
+const handleMessageChange = (event) => {
+  setMessage(event.target.value)
+}
+
+/////////////////////////
+
   if (token) {
     return (
       <>
         <h2>Posts</h2>
-        <button onClick={logout}>Logout</button>
-        <button onClick={post}>Post</button>
-        <input
-          placeholder="Write your post here"
-          id="post"
-          type="text"
-          defaultValue={post}
-        />
+        <button onClick={logout}>Logout</button><br></br>
+          <form onSubmit={handleSubmitPost}>
+          <input placeholder="Write your post here" id="message" type='message' defaultValue={post} onChange={handleMessageChange} />
+          <input id='submit' type="submit" value="Submit" />
+         </form>
+
         <div id="feed" role="feed">
           {posts.map((post) => (
             <Post post={post} key={post._id} />
