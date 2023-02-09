a71236d86678399e7c612352279ea62f10482bbd
Marta Bianchini <marta.bianchini24@gmail.com>
2023-02-09 16:45:01 +0000

    added delete button with functionality

--- frontend/src/components/feed/Feed.js
+++ frontend/src/components/feed/Feed.js
@@ -4,10 +4,7 @@ import Post from "../post/Post";
 const Feed = ({ navigate }) => {
   const [posts, setPosts] = useState([]);
   const [token, setToken] = useState(window.localStorage.getItem("token"));
-
-/////////////////////////
-// This below is code from the exisiting codebase, it might be that it can post messages and the new code below ** is unnecessary.
-/////////////////////////
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
     if (token) {
@@ -30,73 +27,89 @@ const Feed = ({ navigate }) => {
     navigate("/login");
   };
 
-  const post = () => {};  /// Does something need to be added to this? Seems important aas the page doesn't work when it's commented out
+  const post = () => {};
 
-/////////////////////////
-// **This below is new code which handles the fetch to api, sending the new post to the database 
-/////////////////////////
+  /////////////////////////
+  // **This below is new code which handles the fetch to api, sending the new post to the database and deleting it
+  /////////////////////////
 
-const [message, setMessage] = useState("");
+  const handleSubmitPost = async (event) => {
+    // event.preventDefault(); This line stops the page refreshing automatically so it has been commented out
 
-const handleSubmitPost = async (event) => {
-  // event.preventDefault(); This line stops the page refreshing automatically so it has been commented out
-
-  fetch( '/posts', {
-    method: 'post',
-    headers: {
-      Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ message: message })
-  })
-    .then(response => {
-      if(response.status === 201) {
-        navigate('/posts')
+    fetch("/posts", {
+      method: "post",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ message: message }),
+    }).then((response) => {
+      if (response.status === 201) {
+        navigate("/posts");
       } else {
-        navigate('/signup')
+        navigate("/signup");
       }
-    })
-}
+    });
+  };
 
+  const handleMessageChange = (event) => {
+    setMessage(event.target.value);
+  };
 
-/////////////////////////
-// This below is new code
-/////////////////////////
+  const handleDeletePost = async (id) => {
+    await fetch(`/posts/${id}`, {
+      method: "delete",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    }).then((response) => {
+      if (response.status === 200) {
+        setPosts(posts.filter((post) => post._id !== id));
+      }
+    });
+  };
 
-const handleMessageChange = (event) => {
-  setMessage(event.target.value)
-}
-
-/////////////////////////
-// Below is the code for the form that posts the new message
-/////////////////////////
+  /////////////////////////
+  // Below is the code for the form that posts the new message
+  /////////////////////////
   if (token) {
     return (
       <div>
-      <nav className="nav">
+        <nav className="nav">
           <a href="/posts" className="site-title">
             Acebook
           </a>
           <ul>
-            
-            <button onClick={logout}>Logout</button><br></br>
+            <button onClick={logout}>Logout</button>
+            <br></br>
             {/* <a href="/signup"> logout </a> */}
-           
           </ul>
         </nav>
-      <>
-        <h2>Posts</h2>
+        <>
+          <h2>Posts</h2>
           <form onSubmit={handleSubmitPost}>
-          <input placeholder="Write your post here" id="message" type='message' defaultValue={post} onChange={handleMessageChange} />
-          <input id='submit' type="submit" value="Submit" />
-         </form>
+            <input
+              placeholder="Write your post here"
+              id="message"
+              type="message"
+              defaultValue={post}
+              onChange={handleMessageChange}
+            />
+            <input id="submit" type="submit" value="Submit" />
+          </form>
 
-        <div id="feed" role="feed">
-          {posts.map((post) => (
-            <Post post={post} key={post._id} />
-          ))}
-        </div>
-      </>
+          <div id="feed" role="feed">
+            {posts.map((post) => (
+              <div key={post._id}>
+                <Post post={post} />
+                <button onClick={() => handleDeletePost(post._id)}>
+                  Delete
+                </button>
+              </div>
+            ))}
+          </div>
+        </>
       </div>
     );
   } else {
